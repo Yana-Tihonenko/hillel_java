ba0ee483e3ede7c2612c1e266ed846dffa878745
Yana-Tihonenko <tihonenkoyana@gmail.com>
2019-12-11 18:50:15 +0200

    HW3

--- HW44/src/mylen/Car.java
+++ HW44/src/mylen/Car.java
@@ -4,19 +4,27 @@ public class Car {
     int id;
     String mark;
     String model;
-    int year_release;
+    int yearRelease;
     String color;
     Double price;
-    String registration_number;
+    String registrationNumber;
 
-    public Car(int id, String mark, String model, int year_release, String color, Double price, String registration_number) {
+    public Car(int id, String mark, String model, int yeaRelease, String color, Double price, String registrationNumber) {
         this.id = id;
         this.mark = mark;
         this.model = model;
-        this.year_release = year_release;
+        this.yearRelease = yearRelease;
         this.color = color;
         this.price = price;
-        this.registration_number = registration_number;
+        this.registrationNumber = registrationNumber;
+    }
+     void printCar () {
+        System.out.println("id=" + this.id + "; " +
+                "year_release=" + this.yearRelease + "; " +
+                "price=" + this.price + "; " +
+                "mark=" + this.mark + "; " +
+                "color=" + this.color + "; " +
+                "registration_number=" + this.registrationNumber);
     }
 
 }
