e04c1e240501f547e0600a00974f7913645bd9d0
Yana-Tihonenko <tihonenkoyana@gmail.com>
2020-02-29 22:44:18 +0200

    hw18

---
+++ hw18/src/hw18/Main.java
@@ -0,0 +1,41 @@
+package hw18;
+
+import hw18.service.DBManager;
+import hw18.videolibrary.Actor;
+import hw18.videolibrary.Film;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //task1
+        System.out.println("task1");
+        List<Film> film = DBManager.filmOneTwoReleaseLate();
+        for (Film obj : film) {
+            System.out.println(obj.toString());
+        }
+        //task2
+        System.out.println("task2");
+        List<Actor> actors = DBManager.actorToFilm(6);
+        for (Actor obj : actors) {
+            System.out.println(obj.toString());
+        }
+        //task3
+        System.out.println("task3");
+        actors = DBManager.actorMoreCountNToFilms(1);
+        for (Actor obj : actors) {
+            System.out.println(obj.toString());
+        }
+        //task4
+        System.out.println("task4");
+        actors = DBManager.actorToBeProducer();
+        for (Actor obj : actors) {
+            System.out.println(obj.toString());
+        }
+        //task5
+        System.out.println("task5");
+        System.out.println (DBManager.deleteFilmToYear(25));
+    }
+}
---
+++ hw18/src/hw18/connectiontodb/ConectionMySQL.java
@@ -0,0 +1,23 @@
+package hw18.connectiontodb;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConectionMySQL {
+    public static Connection getMySQLConnection()
+            throws ClassNotFoundException, SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        String hostName = "localhost";
+        String dbName = "videolybrary";
+        String userName = "root";
+        String password = "123123";
+        return getMySQLConnection(hostName, dbName, userName, password);
+    }
+
+    private static Connection getMySQLConnection(String hostName, String dbName, String userName, String password) throws SQLException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Class.forName("com.mysql.jdbc.Driver").getDeclaredConstructor().newInstance();
+        String connectionURL ="jdbc:mysql://localhost/videolybrary?useUnicode=true&serverTimezone=UTC&useSSL=true&verifyServerCertificate=false";
+        Connection conectn = DriverManager.getConnection(connectionURL, userName, password);
+        return conectn;
+    }
+}
