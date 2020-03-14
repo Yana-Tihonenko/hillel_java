3b29e999cdaddba37249f403b4332195c88d1bca
Yana-Tihonenko <tihonenkoyana@gmail.com>
2020-03-14 15:59:58 +0200

    hw20

--- hw20/src/main/java/service/FindWordFromFile.java
+++ hw20/src/main/java/service/FindWordFromFile.java
@@ -2,6 +2,7 @@ package service;
 
 
 import java.io.IOException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -9,8 +10,9 @@ import java.util.stream.Stream;
 
 public class FindWordFromFile {
     public static int findWordToURL(String url, String searchWord) throws IOException {
-        String text = new String(Files.readAllBytes(Paths.get("url")), StandardCharsets.UTF_8);
+        String text = new String(Files.readAllBytes(Paths.get(url)), StandardCharsets.UTF_8);
         int count = (int) Stream.of(text.split("[\\P{L}]+")).filter(s -> s.equals(searchWord)).count();
         return count;
     }
+    //
 }
--- hw20/src/main/java/servlets/FindWordEnteredByUser.java
+++ hw20/src/main/java/servlets/FindWordEnteredByUser.java
@@ -12,12 +12,12 @@ import static service.FindWordFromFile.findWordToURL;
 
 //@WebServlet("/view/findword.jsp")
 public class FindWordEnteredByUser extends HttpServlet {
-
+    private static final String FILE="file.txt";
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletContext servletContext = req.getServletContext();
-        String path = (servletContext.getRealPath("/file.txt"));
+        String path = (servletContext.getRealPath("/"))+FILE;
         int count = findWordToURL(path, req.getParameter("findword"));
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
